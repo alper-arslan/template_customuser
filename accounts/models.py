441d708cb7ccd1932d2f7e2f41a26464c2137046
alperarslan <aarslan@gmail.com>
2022-07-13 18:37:00 +0300

    Added CustomUser

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name} || {self.email} || {self.is_superuser}'
+
